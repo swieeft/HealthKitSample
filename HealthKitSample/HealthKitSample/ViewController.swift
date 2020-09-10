485329171a67a8372ee640c32f0d3045a4e8331a
swieeft <swieeft89@gmail.com>
2020-09-10 09:29:19 +0900

    - 헬스킷 샘플 프로젝트 초기 커밋

---
+++ HealthKitSample/HealthKitSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HealthKitSample
+//
+//  Created by Park GilNam on 2020/09/10.
+//  Copyright © 2020 swieeft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
